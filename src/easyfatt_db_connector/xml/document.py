77afe7a5bbc0a5dad9110d0b4ffa721269344a7f
Luca Salvarani <lucasalvarani99@gmail.com>
2023-07-17 16:22:33 +0200

    feat(document): Add customer, transport and delivery objects

--- src/easyfatt_db_connector/xml/document.py
+++ src/easyfatt_db_connector/xml/document.py
@@ -7,7 +7,7 @@ except ImportError:
     from typing_extensions import TypeAlias
 
 from easyfatt_db_connector.xml.product import Product
-from easyfatt_db_connector.xml.common import Field, XMLMapper
+from easyfatt_db_connector.xml.common import Field, FieldGroup, XMLMapper
 
 DocumentType: TypeAlias = Literal[
     "A",
@@ -86,6 +86,418 @@ class Payment(XMLMapper):
         </Payment>
         ```
     """
+    
+@dataclass(init=False, repr=False)
+class DeliveryInfo(XMLMapper):
+	""" Campi nelle righe di consegna (elementi `<Delivery*>`).
+    
+    I campi "Delivery" si riferiscono all'indirizzo di consegna o spedizione e
+	vanno specificati se tale indirizzo differisce da quello nei precedenti campi "Customer".
+	"""
+    
+	__xml_name__ = ""
+        
+	__xml_mapping__ = {
+		"name": "DeliveryName",
+		"address": "DeliveryAddress",
+		"postcode": "DeliveryPostcode",
+		"city": "DeliveryCity",
+		"province": "DeliveryProvince",
+		"country": "DeliveryCountry",
+	}
+
+	name: str
+	""" Nome e cognome o denominazione.
+	
+	Example:
+		```xml
+		<DeliveryName>...</DeliveryName>
+		```
+	"""
+
+	address: str
+	""" Indirizzo.
+	
+	Example:
+		```xml
+		<DeliveryAddress>...</DeliveryAddress>
+		```
+	"""
+
+	postcode: str
+	""" CAP.
+	
+	Example:
+		```xml
+		<DeliveryPostcode>...</DeliveryPostcode>
+		```
+	"""
+
+	city: str
+	""" Città.
+	
+	Example:
+		```xml
+		<DeliveryCity>...</DeliveryCity>
+		```
+	"""
+
+	province: str
+	""" Provincia (2 caratteri).
+	
+	Example:
+		```xml
+		<DeliveryProvince>...</DeliveryProvince>
+		```
+	"""
+
+	country: str
+	""" Nazione.
+	
+	Example:
+		```xml
+		<DeliveryCountry>...</DeliveryCountry>
+		```
+	"""
+
+@dataclass(init=False, repr=False)
+class TransportInfo(XMLMapper):
+    """ Campi nelle righe di trasporto (elementi `<Transport*>`, `<Shipment>`, ecc). """
+
+    __xml_mapping__ = {
+        "carrier": "Carrier",
+        "reason": "TransportReason",
+        "goods_appearance": "GoodsAppearance",
+        "pieces": "NumOfPieces",
+        "date_time": "TransportDateTime",
+        "shipment_terms": "ShipmentTerms",
+        "weight": "TransportedWeight",
+        "tracking_number": "TrackingNumber",
+    }
+
+    carrier: str
+    """ Denominazione vettore. """
+
+    reason: str
+    """ Causale trasporto. """
+
+    goods_appearance: str
+    """ Aspetto delle merci. """
+
+    pieces: int
+    """ Numero colli. """
+
+    date_time: str
+    """ Data e ora del trasporto. """
+
+    shipment_terms: str
+    """ Porto (franco, assegnato...). """
+
+    weight: str
+    """ Peso trasportato. """
+
+    tracking_number: str
+    """ Numero di tracciatura spedizione. """
+
+@dataclass(init=False, repr=False)
+class CustomerInfo(XMLMapper):
+    """ Campi del'intestatario del documento. 
+    
+    Va notato che i campi "Customer" (ovvero ... "Cliente") conterranno invece i dati del fornitore per i documenti di fornitura (ordine cliente, arrivo merce, etc.).
+    """
+
+    __xml_mapping__ = {
+        "code": "CustomerCode",
+        "web_login": "CustomerWebLogin",
+        "name": "CustomerName",
+        "address": "CustomerAddress",
+        "postcode": "CustomerPostcode",
+        "city": "CustomerCity",
+        "province": "CustomerProvince",
+        "country": "CustomerCountry",
+        "fiscal_code": "CustomerFiscalCode",
+        "vat_code": "CustomerVatCode",
+        "e_invoice_dest_code": "CustomerEInvoiceDestCode",
+        "telephone": "CustomerTel",
+        "mobile_phone": "CustomerCellPhone",
+        "fax": "CustomerFax",
+        "email": "CustomerEmail",
+        "pec": "CustomerPec",
+        "reference": "CustomerReference",
+    }
+
+    code: str
+    """ Codice anagrafica; serve per associare l'ordine all'eventuale cliente già inserito in anagrafica. 
+    
+    Durante l'importazione, in presenza di `<CustomerCode>` e in assenza altri codici `<Customer...>`, 
+    i relativi campi anagrafica del documento saranno riempiti con i dati dell'anagrafica corrispondente 
+    a `<CustomerCode>` e già codificata nell'archivio.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerCode>...</CustomerCode>
+        </Document>
+        ```
+    """
+
+    web_login: str
+    """ Login web del cliente (usato nell'integrazione e-commerce).
+     
+    Nella fase di identificazione dell'anagrafica, questo campo viene preso in considerazione solo in mancanza di `CustomerCode`.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerWebLogin>...</CustomerWebLogin>
+        </Document>
+        ```
+    """
+
+    name: str
+    """ Cognome e nome o denominazione sociale.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerName>...</CustomerName>
+        </Document>
+        ```
+    """
+
+    address: str
+    """ Indirizzo.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerAddress>...</CustomerAddress>
+        </Document>
+        ```
+    """
+
+    postcode: str
+    """ CAP.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerPostcode>...</CustomerPostcode>
+        </Document>
+        ```
+    """
+
+    city: str
+    """ Città.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerCity>...</CustomerCity>
+        </Document>
+        ```
+    """
+
+    province: str
+    """ Provincia (2 caratteri).
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerProvince>...</CustomerProvince>
+        </Document>
+        ```
+    """
+
+    country: str
+    """ Nazione.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerCountry>...</CustomerCountry>
+        </Document>
+        ```
+    """
+
+    fiscal_code: str
+    """ Codice fiscale.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerFiscalCode>...</CustomerFiscalCode>
+        </Document>
+        ```
+    """
+
+    vat_code: str
+    """ Partita Iva.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerVatCode>...</CustomerVatCode>
+        </Document>
+        ```
+    """
+
+    e_invoice_dest_code: str
+    """ Codice destinatario o PEC per l'invio della fattura elettronica.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerEInvoiceDestCode>...</CustomerEInvoiceDestCode>
+        </Document>
+        ```
+    """
+
+    telephone: str
+    """ Numero di telefono.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerTel>...</CustomerTel>
+        </Document>
+        ```
+    """
+
+    mobile_phone: str
+    """ Numero di cellulare.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerCellPhone>...</CustomerCellPhone>
+        </Document>
+        ```
+    """
+
+    fax: str
+    """ Numero di fax.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerFax>...</CustomerFax>
+        </Document>
+        ```
+    """
+
+    email: str
+    """ Indirizzo e-mail.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerEmail>...</CustomerEmail>
+        </Document>
+        ```
+    """
+
+    pec: str    
+    """ Indirizzo PEC.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerPec>...</CustomerPec>
+        </Document>
+        ```
+    """
+    
+    reference: str
+    """ Persona di riferimento.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerReference>...</CustomerReference>
+        </Document>
+        ```
+    """
+
+@dataclass(init=False, repr=False)
+class DocumentNotes(XMLMapper):
+    """ Campi note/commenti del documento. """
+
+    __xml_mapping__ = {
+        "internal_comment": "InternalComment",
+        "custom1": "CustomField1",
+        "custom2": "CustomField2",
+        "custom3": "CustomField3",
+        "custom4": "CustomField4",
+        "foot_note": "FootNotes",
+    }
+
+    internal_comment: str = ""
+    """ Commento (nell'e-commerce, usare questo campo per indicare note libere dell'acquirente durante la fase dell'ordine).
+
+    Example:
+        ```xml
+        <Document>
+            <InternalComment>...</InternalComment>
+        </Document>
+        ```
+    """
+
+    custom1: str = ""
+    """ Campo note 1.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomField1>...</CustomField1>
+        </Document>
+        ```
+    """
+
+    custom2: str = ""
+    """ Campo note 2.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomField2>...</CustomField2>
+        </Document>
+        ```
+    """
+
+    custom3: str = ""
+    """ Campo note 3.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomField3>...</CustomField3>
+        </Document>
+        ```
+    """
+
+    custom4: str = ""
+    """ Campo note 4.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomField4>...</CustomField4>
+        </Document>
+        ```
+    """
+
+    foot_note: str = ""
+    """ Note a fine pagina.
+
+    Example:
+        ```xml
+        <Document>
+            <FootNotes>...</CustomerCode>
+        </Document>
+        ```
+    """
+
 
 
 @dataclass(init=False, repr=False)
@@ -102,6 +514,11 @@ class Document(XMLMapper):
         "payments": Field(tag="Payments", is_parent=True, child=Field(Payment)),
         "numbering": "Numbering",
         "type": "DocumentType",
+
+        "delivery": FieldGroup(DeliveryInfo),
+        "transport": FieldGroup(TransportInfo),
+        "customer": FieldGroup(CustomerInfo),
+        "notes": FieldGroup(DocumentNotes),
     }
 
     rows: list["Product"]
@@ -166,6 +583,74 @@ class Document(XMLMapper):
         ```xml
         <Document>
             <DocumentType>C</DocumentType>
+        </Document>
+        ```
+    """
+
+    delivery: Optional[DeliveryInfo] = None
+    """ Informazioni aggiuntive sulla consegna o spedizione.
+
+    Example:
+        ```xml
+        <Document>
+            <DeliveryName>...</DeliveryName>
+            <DeliveryAddress>...</DeliveryAddress>
+            <DeliveryPostcode>...</DeliveryPostcode>
+            <DeliveryCity>...</DeliveryCity>
+            <DeliveryProvince>...</DeliveryProvince>
+            <DeliveryCountry>...</DeliveryCountry>
+        </Document>
+        ```
+    """
+
+    transport: Optional[TransportInfo] = None
+    """ Informazioni aggiuntive sul trasporto.
+
+    Example:
+        ```xml
+        <Document>
+            <Carrier>...</Carrier>
+            <TransportReason>...</TransportReason>
+            <GoodsAppearance>...</GoodsAppearance>
+            <NumOfPieces>...</NumOfPieces>
+            <TransportDateTime>...</TransportDateTime>
+            <ShipmentTerms>...</ShipmentTerms>
+            <TransportedWeight>...</TransportedWeight>
+            <TrackingNumber>...</TrackingNumber>
+        </Document>
+        ```
+    """
+
+    customer: Optional[CustomerInfo] = None
+    """ Informazioni aggiuntive sull'intestatario del documento.
+    
+    Questo può essere cliente o fornitore a seconda del tipo di documento.
+
+    Example:
+        ```xml
+        <Document>
+            <CustomerCode>...</CustomerCode>
+            <CustomerWebLogin>...</CustomerWebLogin>
+            <CustomerName>...</CustomerName>
+            <CustomerAddress>...</CustomerAddress>
+            <CustomerVatCode>...</CustomerVatCode>
+            <CustomerEmail>...</CustomerEmail>
+        </Document>
+        ```
+    """
+
+    notes: Optional[DocumentNotes] = None
+    """ Note aggiuntive sul documento.
+
+    Example:
+        ```xml
+        <Document>
+            <InternalComment>...</InternalComment>
+            <CustomField1>...</CustomField1>
+            <CustomField2>...</CustomField2>
+            <CustomField3>...</CustomField3>
+            <CustomField4>...</CustomField4>
+            <FootNotes>...</FootNotes>
         </Document>
         ```
     """
