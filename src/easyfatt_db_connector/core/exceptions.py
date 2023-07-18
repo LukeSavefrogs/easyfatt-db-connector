c1e03125116aa698fa178d0f8f42f9453173f428
Luca Salvarani <lucasalvarani99@gmail.com>
2023-07-18 18:26:12 +0200

    feat(exceptions): Add `TypeConversionError` exception

--- src/easyfatt_db_connector/core/exceptions.py
+++ src/easyfatt_db_connector/core/exceptions.py
@@ -5,8 +5,15 @@ class EasyfattDBConnectorError(Exception):
     """A base class for exceptions coming from the `easyfatt-db-connector` project."""
 
 
+class EasyfattXMLError(Exception):
+    """A base class for exceptions coming from the `easyfatt-db-connector.xml` project."""
+
+
 class FirebirdClientError(EasyfattDBConnectorError):
     """An error concerning the Firebird client."""
 
 class DatabaseLockedError(EasyfattDBConnectorError):
     """ Error raised when the Firebird database is locked. """
+
+class TypeConversionError(EasyfattXMLError):
+    """ Error raised when a type conversion of an XML field is not possible. """
